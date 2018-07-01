d46a2526242cf88cd8bdc8db9424afddff1a0973
Kayla Reopelle <kreopelle@gmail.com>
2018-07-01 01:18:20 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,8 +5,8 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.order(rating: :desc).first
-    #Show.where("rating = ?", Show.highest_rating)
+    #Show.order(rating: :desc).first
+    Show.where("rating = ?", Show.highest_rating)
   end
 
   def self.lowest_rating
