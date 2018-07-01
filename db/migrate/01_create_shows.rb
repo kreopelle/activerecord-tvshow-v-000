08a127d9f7237e592ba1c7a7b275da8b9bad5941
Kayla Reopelle <kreopelle@gmail.com>
2018-07-01 00:52:58 +0000

    automatically backed up by learn

--- db/migrate/01_create_shows.rb
+++ db/migrate/01_create_shows.rb
@@ -1,10 +1,10 @@
-class CreateShows < ActiveRecord::Migration[4.2]
+class CreateShows < ActiveRecord::Migration
   def change
     create_table :shows do |t|
       t.string :name
       t.string :network
       t.string :day
       t.integer :rating
-    end 
+    end
   end
 end
